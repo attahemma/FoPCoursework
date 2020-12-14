a27ddf1115811ac6b31353995a2306d5420981aa
Attah Emmanuel <attahemma0@gmail.com>
2020-12-14 12:52:47 +0100

    first new commit

---
+++ src/uk/ac/bradford/dungeongame/Clock.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.bradford.dungeongame;
+
+/**
+ *
+ * @author GCONAM
+ */
+public class Clock {
+    private static boolean pause = false;
+    public static long lastFrame, totalTime;
+    public static float d = 0, multiplier=1;
+}
